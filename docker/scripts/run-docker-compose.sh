e9c9aad49bf34614f2fe68dd127e481c35bc69af
David Engelmann <david+4113@satisfilabs.com>
2023-08-16 15:49:15 -0400

    add section for build in run-docker-compose.sh

--- docker/scripts/run-docker-compose.sh
+++ docker/scripts/run-docker-compose.sh
@@ -7,5 +7,6 @@ cp deploy-sourcegraph-docker/caddy/builtins/http.Caddyfile /tmp/http.Caddyfile
 cd deploy-sourcegraph-docker
 
 # Run Docker Compose
+docker compose -f docker-compose/docker-compose.yaml build
 docker compose -f docker-compose/docker-compose.yaml up -d
 exec "$@"
