9865878b5a01fd3a99b21d37fd5436f115beabf3
David Engelmann <david+4113@satisfilabs.com>
2023-08-25 10:19:15 -0400

    add in python dap and python VIRTUAL_ENV var

--- config/lua/custom/plugins/lsp.lua
+++ config/lua/custom/plugins/lsp.lua
@@ -59,7 +59,7 @@ return {
 
   "simrat39/inlay-hints.nvim",
   { "j-hui/fidget.nvim", branch = "legacy" },
-  "folke/neodev.nvim",
+  {"folke/neodev.nvim", opts = {}},
   "jose-elias-alvarez/null-ls.nvim",
   "jose-elias-alvarez/nvim-lsp-ts-utils",
   "scalameta/nvim-metals",
